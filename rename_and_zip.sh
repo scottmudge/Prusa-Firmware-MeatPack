9ec74549a16363071e5dbb34ebda0e21d9d493e0
vertigo235 <jbjonesclt@gmail.com>
2020-10-11 12:21:59 -0400

    Include Build # in Rename Script
    * Update Drone Workspace for rename_and_zip.sh

--- rename_and_zip.sh
+++ rename_and_zip.sh
@@ -9,34 +9,35 @@
 SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 CURRENTDIR="$( pwd )"
 TODAY=$(date +'%Y%m%d')
-HEXDIR="$CI_WORKSPACE/out"    # absolute link
+HEXDIR="$DRONE_WORKSPACE/out"    # absolute link
 
 cd "${HEXDIR}"
 
-# Rename MK3S hex file & GET FW 
+# Rename MK3S hex file & GET FW
 hexfile=$(ls *Build*MK3S-EINSy10a*.hex)
-newhexfile=$(echo ${hexfile} | awk -F[-_] '{ printf("Bear_Cal_%s-%s-%s", $1, $6, $7) }' )
-fwbuild=$(echo ${hexfile} | awk -F[-_] '{ printf("%s", $1) }' )
+fwbuild=$(echo ${hexfile} | sed 's/^FW//; s/-Build/-B/' | awk -F[-_] '{ printf("%s-%s", $1, $2) }' )
+newhexfile=$(echo ${hexfile} | awk -F[-_] -v fwb="$fwbuild" '{ printf("BearFW_Cal_%s_%s-%s", fwb, $6, $7) }' )
 echo "${hexfile}  -->  ${newhexfile}"
 mv ${hexfile} ${newhexfile}
 
 # Rename and ZIP MK2.5S hex files for Rambo 13a
 for hexfile in $(ls *Build*MK25S-RAMBo13a*.hex); do
-    newhexfile=$(echo ${hexfile} | awk -F[-_] '{ printf("Bear_Cal_%s-%s-%s-%s", $1, $6, $7, $8) }' )
+    newhexfile=$(echo ${hexfile} | awk -F[-_] -v fwb="$fwbuild" '{ printf("BearFW_Cal_%s_%s-%s-%s", fwb, $6, $7, $8) }' )
     echo "${hexfile}  -->  ${newhexfile}"
     mv ${hexfile} ${newhexfile}
 done
+
 echo "create zip..."
-zip -mq Bear_Cal_${fwbuild}_MK25S-RAMBo13a.zip *MK25S-RAMBo13a*.hex
+zip -mq BearFW_Cal_${fwbuild}_MK25S-RAMBo13a.zip *MK25S-RAMBo13a*.hex
 
 # Rename and ZIP MK2.5S hex files for Rambo 10a
 for hexfile in $(ls *Build*MK25S-RAMBo10a*.hex); do
-    newhexfile=$(echo ${hexfile} | awk -F[-_] '{ printf("Bear_Cal_%s-%s-%s-%s", $1, $6, $7, $8) }' )
+    newhexfile=$(echo ${hexfile} | awk -F[-_] -v fwb="$fwbuild" '{ printf("BearFW_Cal_%s_%s-%s-%s", fwb, $6, $7, $8) }' )
     echo "${hexfile}  -->  ${newhexfile}"
     mv ${hexfile} ${newhexfile}
 done
 echo "create zip..."
-zip -mq Bear_Cal_${fwbuild}_MK25S-RAMBo10a.zip *MK25S-RAMBo10a*.hex
+zip -mq BearFW_Cal_${fwbuild}_MK25S-RAMBo10a.zip *MK25S-RAMBo10a*.hex
 
 ls *.*
 
