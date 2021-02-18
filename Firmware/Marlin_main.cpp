bc0a1f4904b6e952e08cc57e2a746c33501e0a93
Scott Mudge <mail@scottmudge.com>
2021-02-18 14:52:52 -0500

    Fix

--- Firmware/Marlin_main.cpp
+++ Firmware/Marlin_main.cpp
@@ -761,8 +761,11 @@ static void factory_reset(char level)
 		// Force the "Follow calibration flow" message at the next boot up.
 		calibration_status_store(CALIBRATION_STATUS_Z_CALIBRATION);
 		eeprom_write_byte((uint8_t*)EEPROM_WIZARD_ACTIVE, 1); //run wizard
+		
+#ifndef DISABLE_FARM_MODE
 		farm_mode = false;
 		eeprom_update_byte((uint8_t*)EEPROM_FARM_MODE, farm_mode);
+#endif
 
 #ifdef FILAMENT_SENSOR
 		fsensor_enable();
